c8fd87844f20f8180f6db7dfee4325810d5334c2
yzx929 <1549758049@qq.com>
2024-12-12 17:40:48 +0800

    feat:table增加添加数据功能

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./index.less";
 import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
 
 export default function Layout() {
   const usersDate = [
@@ -7,49 +8,84 @@ export default function Layout() {
     { id: 2, name: "李四", phone: "0987654321", email: "lisi@example.com" },
     { id: 3, name: "王五", phone: "1234567890", email: "zhangsan@example.com" },
     { id: 4, name: "猪八戒", phone: "0987654321", email: "lisi@example.com" },
-    { id: 1, name: "张三", phone: "1234567890", email: "zhangsan@example.com" },
     { id: 5, name: "沙和尚", phone: "0987654321", email: "lisi@example.com" },
   ];
   const [users, setUsers] = useState(usersDate);
+  const onFinish = (values) => {
+    const newUserItem = {
+      id: users.length + 1,
+      name: values?.name,
+      phone: values?.phone,
+      email: values?.email,
+    };
+
+    // const pushUsers = [...users]
+    // pushUsers.push(newUserItem)
+    const newUsers = [...users];
+    console.log("newUsers:",newUsers);
+    newUsers.push(newUserItem);
+    setUsers(newUsers);
+  };
   return (
     <div className={styles.tableWrapper}>
-      <table>
-        <thead>
-          <tr>
-            <th>姓名</th>
-            <th>电话</th>
-            <th>邮箱</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.phone}</td>
-              <td>{item.email}</td>
+      <Form
+        onFinish={onFinish} // 表单提交时触发
+        layout="vertical"
+      >
+        <table>
+          <thead>
+            <tr>
+              <th>姓名</th>
+              <th>电话</th>
+              <th>邮箱</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+                <td>
+                  <button>删除</button>
+                </td>
+              </tr>
+            ))}
+            <tr>
               <td>
-                <button>删除</button>
+                <Form.Item
+                  name="name"
+                  //rules={[{ required: true, message: "请输入名字!" }]}
+                >
+                  <Input />
+                </Form.Item>
+              </td>
+              <td>
+                <Form.Item name="phone">
+                  <Input />
+                </Form.Item>
+              </td>
+              <td>
+                <Form.Item name="email">
+                  <Input />
+                </Form.Item>
+              </td>
+              <td>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ color: "#fff" }}
+                  >
+                    添加
+                  </Button>
+                </Form.Item>
               </td>
             </tr>
-          ))}
-          <tr>
-            <td>
-              <input type="text" />
-            </td>
-            <td>
-              <input type="tel" />
-            </td>
-            <td>
-              <input type="email" />
-            </td>
-            <td>
-              <button>添加</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
+          </tbody>
+        </table>
+      </Form>
       {/* <form onSubmit={handleSubmit}>
         <label for="name">名字:</label>
         <input onChange={changeHandler} type="text" id="name" name="name" />
